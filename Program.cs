e6506882ab98104efb4d3c846d3b2e1e33b19ad0
Remmoze <remmoze@gmail.com>
2021-10-06 12:13:10 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+
+namespace CustomRunCommands
+{
+    class Program
+    {
+        static public void Main(String[] args)
+        {
+            var arguments = GetArguments(args);
+            if (arguments == null) return;
+
+            var (shortcut, location) = arguments;
+            var storage = new Storage();
+
+        }
+
+        static public Tuple<string, string> GetArguments(String[] args)
+        {
+            Console.WriteLine();
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: \ncrc <shortcut> <location>");
+                return null;
+            }
+            var shortcut = args[0];
+            var location = Path.GetFullPath(args[1]);
+
+            Console.WriteLine("Provided arguments:");
+            Console.WriteLine();
+            Console.WriteLine("Shortcut:\t" + shortcut);
+            Console.WriteLine("Location:\t" + location);
+            Console.WriteLine();
+
+            Console.Write("Is this correct? (y/n): ");
+            if (Console.ReadLine() != "y")
+            {
+                Console.WriteLine("Well be careful next time.");
+                return null;
+            }
+            return new Tuple<string, string>(shortcut, location);
+        }
+    }
+}
---
+++ Storage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace CustomRunCommands
+{
+    class JsonStorage
+    {
+        public DateTime CreationDate { get; set; }
+        public List<Shortcut> Shortcuts { get; set; }
+    }
+
+    class Shortcut
+    {
+        public string ShortName { get; }
+        public string Path { get; }
+    }
+
+    public class Storage
+    {
+        const string StorageDirectoryPath = @"C:/PATH";
+        const string StorageFilePath = StorageDirectoryPath + "/.path.json";
+
+        private JsonStorage Data = new JsonStorage();
+
+        public Storage()
+        {
+            if (!Directory.Exists(StorageDirectoryPath))
+            {
+                Directory.CreateDirectory(StorageDirectoryPath);
+            }
+            if (!File.Exists(StorageFilePath))
+            {
+                using (FileStream fs = File.Create(StorageFilePath))
+                {
+                    var data = JsonSerializer.Serialize(Data, new()
+                    {
+                        WriteIndented = true,
+                    });
+                    var info = new UTF8Encoding(true).GetBytes(data);
+                    fs.Write(info, 0, info.Length);
+                }
+                Console.WriteLine("Generated new storage file.");
+            }
+            //LoadData();
+        }
+
+        //private void LoadData()
+        //{
+        //    var data = JObject.Parse(File.ReadAllText(StorageFilePath));
+        //    foreach (JValue shortcut in (JArray)data["shortcuts"])
+        //    {
+        //        Shortcuts.Add(new Shortcut(shortcut));
+        //    }
+        //}
+    }
+}
